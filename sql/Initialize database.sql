29d71b67971fbbfa032ec7bbd3a7b6e6ed38d8bd
sergiobq0123 <s.sanchezp@alumnos.upm.es>
2024-09-17 22:55:40 +0200

    add sql layout

--- sql/Initialize database.sql
+++ sql/Initialize database.sql
@@ -3,15 +3,15 @@ CREATE DATABASE TENIS;
 USE TENIS;
 
 CREATE TABLE IF NOT EXISTS PLAYER(
-    id_player INT auto_increment primary key,
-    player_name VARCHAR(254) NOT NULL UNIQUE
+                                     id_player INT auto_increment primary key,
+                                     player_name VARCHAR(254) NOT NULL UNIQUE
     );
 
 CREATE TABLE IF NOT EXISTS POINT (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_player1 INT NOT NULL,
-    id_player2 INT NOT NULL,
-    score_player1 VARCHAR(255),
+                                     id INT AUTO_INCREMENT PRIMARY KEY,
+                                     id_player1 INT NOT NULL,
+                                     id_player2 INT NOT NULL,
+                                     score_player1 VARCHAR(255),
     score_player2 VARCHAR(255),
     id_player_service INT NOT NULL,
     id_point_winner INT,
@@ -19,31 +19,29 @@ CREATE TABLE IF NOT EXISTS POINT (
     );
 
 CREATE TABLE IF NOT EXISTS GAME (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_player1 INT NOT NULL,
-    id_player2 INT NOT NULL,
-    id_set INT NOT NULL,
-    points_player1 VARCHAR(255),
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
+                                    id_player1 INT NOT NULL,
+                                    id_player2 INT NOT NULL,
+                                    id_set INT NOT NULL,
+                                    points_player1 VARCHAR(255),
     points_player2 VARCHAR(255),
     id_game_winner INT
-);
+    );
 
 CREATE TABLE IF NOT EXISTS SET_ (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_player1 INT NOT NULL,
-    id_player2 INT NOT NULL,
-    id_match INT NOT NULL,
-    games_player1 INT,
-    games_player2 INT,
-    id_set_winner INT
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
+                                    id_player1 INT NOT NULL,
+                                    id_player2 INT NOT NULL,
+                                    id_match INT NOT NULL,
+                                    games_player1 INT,
+                                    games_player2 INT,
+                                    id_set_winner INT
 );
 CREATE TABLE IF NOT EXISTS MATCH_ (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date date NOT NULL,
-    sets_number INT NOT NULL,
-    id_player1 INT NOT NULL,
-    id_player2 INT NOT NULL,
-    sets_player1 INT,
-    sets_player2 INT,
-    id_match_winner INT
+                                      id INT AUTO_INCREMENT PRIMARY KEY,
+                                      date date NOT NULL,
+                                      sets_number INT NOT NULL,
+                                      id_player1 INT NOT NULL,
+                                      id_player2 INT NOT NULL,
+                                      id_match_winner INT
 );
